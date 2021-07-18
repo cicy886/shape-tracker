e1630e4ced258035c370cdc1253f12cb003e44e1
cicy886 <84603339+cicy886@users.noreply.github.com>
2021-07-18 15:38:07 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,3 @@
-// const path = require('path');
-
-// module.exports = {
-//   entry: './src/index.js',
-//   output: {
-//     filename: 'bundle.js',
-//     path: path.resolve(__dirname, 'dist')
-//   }
-// };
-
-// module.exports = {
-//   entry: './src/index.js',
-//   output: {
-//     filename: 'bundle.js',
-//     path: path.resolve(__dirname, 'dist')
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.css$/,
-//         use: [
-//           'style-loader',
-//           'css-loader'
-//         ]
-//       }
-//     ]
-//   }
-// };
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
